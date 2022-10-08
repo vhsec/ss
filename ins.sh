06c4ece5d3f60034ecd05d973bdd5a57133ff1f8
root <root@faisalbek.ngadem.in>
2022-10-08 15:29:25 +0000

    P

--- ins.sh
+++ ins.sh
@@ -3,8 +3,4 @@ cp rsa_host_key /etc/smile/rsa_host_key
 cp ecdsa_host_key /etc/smile/ecdsa_host_key
 cp ed25519_host_key /etc/smile/ed25519_host_key
 echo "Done. Copying key"
-cd dr
-./configure
-make dropbear
-echo "Done. Build dropbear"
 cp dropbear /etc/smile/
